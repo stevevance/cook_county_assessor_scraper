b357832e4b8ae901fbdf9d946f9dbdee91057af3
Steven Vance <steve@stevevance.net>
2017-06-15 17:29:44 -0500

    slow down requests

--- cook_county_assessor_scraper/settings.py
+++ cook_county_assessor_scraper/settings.py
@@ -12,8 +12,8 @@ BOT_NAME = 'cook_county_assessor'
 
 SPIDER_MODULES = ['cook_county_assessor_scraper.spiders']
 NEWSPIDER_MODULE = 'v.spiders'
-DOWNLOAD_DELAY = 0.009
-CONCURRENT_REQUESTS = 30
+DOWNLOAD_DELAY = 0.02
+CONCURRENT_REQUESTS = 25
 MEMDEBUG_ENABLED = True
 DEFAULT_REQUEST_HEADERS = {
     'Referer': 'http://www.cookcountyassessor.com/'
