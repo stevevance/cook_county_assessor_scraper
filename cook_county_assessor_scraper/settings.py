b7c8ff40c1792250f0f70fd57d4363970528621e
Steven Vance <steve@stevevance.net>
2017-06-16 10:21:10 -0500

    increase rate

--- cook_county_assessor_scraper/settings.py
+++ cook_county_assessor_scraper/settings.py
@@ -12,8 +12,8 @@ BOT_NAME = 'cook_county_assessor'
 
 SPIDER_MODULES = ['cook_county_assessor_scraper.spiders']
 NEWSPIDER_MODULE = 'v.spiders'
-DOWNLOAD_DELAY = 0.006
-CONCURRENT_REQUESTS = 36
+DOWNLOAD_DELAY = 0.005
+CONCURRENT_REQUESTS = 39
 MEMDEBUG_ENABLED = True
 DEFAULT_REQUEST_HEADERS = {
     'Referer': 'http://www.cookcountyassessor.com/'
